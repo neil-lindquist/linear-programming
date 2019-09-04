fb8d9f9b4f1451076cc5cb5d7292fc33454c4510
Neil Lindquist <NeilLindquist5@gmail.com>
2019-09-03 20:55:47 -0400

    Add external formats to overall package

--- src/all.lisp
+++ src/all.lisp
@@ -3,9 +3,11 @@
   (:nicknames :linear-programming)
   (:use-reexport :linear-programming/problem
                  :linear-programming/solver
-                 :linear-programming/conditions)
+                 :linear-programming/conditions
+                 :linear-programming/external-formats)
   (:documentation "The overall package for the linear programming library.
                    It contains only the reexported symbols of
                    [LINEAR-PROGRAMMING/PROBLEM](#package-linear-programming/problem),
-                   [LINEAR-PROGRAMMING/SOLVER](#package-linear-programming/solver), and
-                   [LINEAR-PROGRAMMING/CONDITIONS](#package-linear-programming/conditions)."))
+                   [LINEAR-PROGRAMMING/SOLVER](#package-linear-programming/solver),
+                   [LINEAR-PROGRAMMING/CONDITIONS](#package-linear-programming/conditions), and
+                   [LINEAR-PROGRAMMING/EXTERNAL-FORMATS](#package-linear-programming/external-formats)."))
